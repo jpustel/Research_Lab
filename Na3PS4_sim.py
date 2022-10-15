7702116fdba25006ef7cebfef111f4276d686b47
jpustel <jpusteln@ucsd.edu>
2022-10-15 12:15:41 -0700

    Added Na3PS4 Sim

---
+++ Na3PS4_sim.py
@@ -0,0 +1,24 @@
+import warnings
+
+from m3gnet.models import Relaxer
+from pymatgen.core import Lattice, Structure
+
+
+from mp_api.client import MPRester
+with MPRester(api_key="bl5ZA4p8qFoei37Lo61kGU9Yr0JD6TE5") as mpr:
+    data = mpr.materials.get_data_by_id("mp-985584")
+
+for category in (UserWarning, DeprecationWarning):
+    warnings.filterwarnings("ignore", category=category, module="tensorflow")
+
+mo = data.make_supercell((3,3,3))
+
+relaxer = Relaxer()  # This loads the default pre-trained model
+
+relax_results = relaxer.relax(mo, verbose=True)
+
+final_structure = relax_results['final_structure']
+final_energy_per_atom = float(relax_results['trajectory'].energies[-1] / len(mo))
+
+print(f"Relaxed lattice parameter is {final_structure.lattice.abc[0]:.3f} Å")
+print(f"Final energy is {final_energy_per_atom:.3f} eV/atom")
